866acbf8381f9e140964cbee92c0a63f265329a6
Khasim-patan <khasimpatan77@gmail.com>
2025-01-24 20:21:45 +0530

    sample

--- test.sh
+++ test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Read two numbers from the user
+echo "Enter the first number:"
+num1=$1
 
-a=$1
-b=$2
+echo "Enter the second number:"
+num2=$2
+
+# Add the numbers
+sum=$((num1 + num2))
+
+# Output the result
+echo "The sum of $num1 and $num2 is: $sum"
 
 
-echo "$a+$b"
